d69a1142a397ac8a52470aeedafb2e513144c4ce
yulu9206@gmail.com <yulu9206@Lus-MacBook-Air.local>
2018-11-26 18:55:08 -0800

    mc added

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 
 from django.contrib import admin
 
+from django.conf.urls import url
+
 admin.autodiscover()
 
 import hello.views
@@ -15,7 +17,12 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
-    path("admin/", admin.site.urls),
+    url(r'^$', hello.views.index, name='index'),
+    # path('/', hello.views.index, name='index'),
+    path('movies/', hello.views.movies),
+    path('customers/', hello.views.customers),
+    path('login/', hello.views.login),
+    path('customer-detail/', hello.views.customerDetail),
+    path('movie-detail/', hello.views.movieDetail),
+    path('reports/', hello.views.reports),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,19 +1,116 @@
+import os
+# import requests
+
+from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Greeting
 
-# Create your views here.
+api_key = 'Z1P14W088UZ4E700'
+
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+	return render(request, 'homepage.html')
 
+def login(request):
+	return render(request, 'login.html')
 
-def db(request):
+def movies(request):
+	return render(request, 'movies.html')
 
-    greeting = Greeting()
-    greeting.save()
+def movieDetail(request):
+	return render(request, 'movieDetail.html')
 
-    greetings = Greeting.objects.all()
+def customers(request):
+	return render(request, 'customers.html')
 
-    return render(request, "db.html", {"greetings": greetings})
+def customerDetail(request):
+	return render(request, 'customerDetail.html')
+
+def reports(request):
+	return render(request, 'reports.html')
+
+# def db(request):
+# 	greeting = Greeting()
+# 	greeting.save()
+# 	greetings = Greeting.objects.all()
+# 	return render(request, 'db.html', {'greetings': greetings})
+
+# def hw1Web(request):
+# 	results = {
+# 		'symbol': '',
+# 		'proceeds': 0,
+# 		'cost': 0,
+# 		'net_profit': 0,
+# 		'return_on_invest': 0,
+# 		'break_even_price': 0
+# 	}
+# 	return render(request, 'hw1.html', {'results': results})
+
+# def calculate(request):
+# 	postData = {
+# 	'symbol': request.POST.get('symbol', "default symbol"),
+# 	'allotment': request.POST.get('allotment', "default allotment"),
+# 	'final_share': request.POST.get('final_share', "default final_share"),
+# 	'sell_commission': request.POST.get('sell_commission', "default sell_commission"),
+# 	'initial_share': request.POST.get('initial_share', "default initial_share"),
+# 	'buy_commission': request.POST.get('buy_commission', "default buy_commission"),
+# 	'tax_rate': request.POST.get('tax_rate', "default tax_rate"),
+# 	}
+# 	results = profitCalculate(postData)
+# 	return render(request, 'hw1.html', {'results': results})
+
+# def profitCalculate(postData):
+# 	allotment = float(postData['allotment'])
+# 	final_share_price = float(postData['final_share'])
+# 	sell_commission = float(postData['sell_commission'])
+# 	initial_share_price = float(postData['initial_share'])
+# 	buy_commission = float(postData['buy_commission'])
+# 	tax_rate = float(postData['tax_rate'])
+
+# 	proceeds = allotment * final_share_price
+# 	total_purchase_price = allotment * initial_share_price
+# 	capital_gain = (final_share_price - initial_share_price) * allotment - buy_commission - sell_commission 
+# 	tax = capital_gain * tax_rate / 100
+# 	cost = total_purchase_price + buy_commission + sell_commission + tax
+# 	net_profit = proceeds - cost
+# 	return_on_investment = net_profit / cost * 100
+# 	break_even_price = (total_purchase_price + buy_commission + sell_commission) / allotment
+
+# 	results = {
+# 	'symbol': postData['symbol'],
+# 	'proceeds': proceeds,
+# 	'cost': cost,
+# 	'net_profit': net_profit,
+# 	'return_on_investment': return_on_investment,
+# 	'break_even_price': break_even_price
+# 	}
+# 	return results
+
+# def hw2(request):
+# 	results = {
+# 		'symbol': '',
+# 		'proceeds': 0,
+# 		'cost': 0,
+# 		'net_profit': 0,
+# 		'return_on_invest': 0,
+# 		'break_even_price': 0
+# 	}
+# 	return render(request, 'hw2.html', {'results': results})
+
+# def getFinanceInfo(request):
+# 	postData = {
+# 		'symbol': request.POST.get('symbol', "default symbol"),
+# 		'allotment': request.POST.get('allotment', "default allotment"),
+# 		'final_share': request.POST.get('final_share', "default final_share"),
+# 		'sell_commission': request.POST.get('sell_commission', "default sell_commission"),
+# 		'initial_share': request.POST.get('initial_share', "default initial_share"),
+# 		'buy_commission': request.POST.get('buy_commission', "default buy_commission"),
+# 		'tax_rate': request.POST.get('tax_rate', "default tax_rate"),
+# 	}
+# 	results = callFinanceService(postData)
+# 	return render(request, 'hw2.html', {'results': results})
+
+# def callFinanceService(postData):
+# 	data = requests.get('http://alphavantage.co/query?function=apiKey={}'.format(api_key))
+# 	results = data.json()
+# 	return results
