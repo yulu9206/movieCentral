3bb4e8f2969acf53b1e5c119c92d39f0c52ec15b
yulu9206@gmail.com <yulu9206@Lus-MacBook-Air.local>
2018-11-29 19:30:40 -0800

    resoving errors

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "livesync",
+    # "livesyanc",
     "hello",
 ]
 
@@ -53,9 +53,9 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-MIDDLEWARE_CLASSES = (
-    'livesync.core.middleware.DjangoLiveSyncMiddleware',
-)
+# MIDDLEWARE_CLASSES = (
+#     'livesync.core.middleware.DjangoLiveSyncMiddleware',
+# )
 
 ROOT_URLCONF = "gettingstarted.urls"
 
